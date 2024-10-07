6f78c81c6261ebac1e59cba5b23de9c0f1fa62c0
mateusvascaino <163007807+mateusvascaino@users.noreply.github.com>
2024-10-07 11:22:44 -0300

    simsim

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    `Programação 2.0`
+    `O que é phyton`
+    `O phyton é uma linguagem de programação`
+)
+criaCartao(
+    `Futebol`
+    `Aonde Adriano Imperador estreou `
+    `Flamengo`
+)
+criaCartao(
+    `Programação 2.0`
+    `O que é phyton`
+    `O phyton é uma linguagem de programação`
+)
+criaCartao(
+    `Programação 2.0`
+    `O que é phyton`
+    `O phyton é uma linguagem de programação`
+)
+criaCartao(
+    `Programação 2.0`
+    `O que é phyton`
+    `O phyton é uma linguagem de programação`
+)
+criaCartao(
+    `Programação 2.0`
+    `O que é phyton`
+    `O phyton é uma linguagem de programação`
+)

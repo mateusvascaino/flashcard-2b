496065835ab4d83c4e77fcecabf48cc01d4d49e6
mateusvascaino <163007807+mateusvascaino@users.noreply.github.com>
2024-10-15 08:39:00 -0300

    ugi

--- perguntas.js
+++ perguntas.js
@@ -1,20 +1,20 @@
 criaCartao(
     'Programação 2.0'
-    'O que é phyton'
+    'O que é phyton' ,
     'O phyton é uma linguagem de programação'
 )
 criaCartao(
     'Futebol'
-    'Aonde Adriano Imperador estreou '
+    'Aonde Adriano Imperador estreou ' ,
     'Flamengo'
 )
 criaCartao(
     'Programação 2.0'
-    'O que é phyton'
+    'O que é phyton' ,
     'O phyton é uma linguagem de programação'
 )
 criaCartao(
     'Futebol'
-    'Aonde Adriano Imperador estreou '
+    'Aonde Adriano Imperador estreou ' ,
     'Flamengo'
 )
